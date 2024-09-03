be49e9981abf7ab5ab41c9bdccbb0d51b3b0ce97
Olena_Podoliak <megvbasql@gmail.com>
2024-09-03 23:47:17 +0300

    add font

--- WEO/gui.py
+++ WEO/gui.py
@@ -1,11 +1,11 @@
 import FreeSimpleGUI as sg
 inputTest = sg.Text("Choose the file to compare:")
 inputTest1 = sg.Text("File 1:")
-input1 = sg.Input(key = "file1")
+input1 = sg.Input(key = "file1", size=(60,1), font=("Courier New",10))
 choose_button1 = sg.FileBrowse("Choose", key="btnFile1")
 
 inputTest2 = sg.Text("File 2:")
-input2 = sg.Input(key = "file2")
+input2 = sg.Input(key = "file2", size=(60,1), font=("Courier New",10))
 choose_button2 = sg.FileBrowse("Choose", key="btnFile2")
 
 compare_button = sg.Button("Compare", key="btnCompare")
@@ -15,7 +15,7 @@ window = sg.Window("Comparing",
                  [inputTest2, input2, choose_button2],
                  [compare_button]
                  ],
-                font=("Arial",14))
+                font=("Arial",12))
 while True:
     event, values = window.read()
     match event:
